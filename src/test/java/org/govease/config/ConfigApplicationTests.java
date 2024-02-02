9b091d04f82f6351f19a493135ee431b36cf9043
ouharri <ouharrioutman@gmail.com>
2024-02-02 17:01:38 +0000

    setup config service

---
+++ src/test/java/org/govease/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package org.govease.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
